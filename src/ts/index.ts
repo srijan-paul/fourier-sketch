dfab529669af3c9ac1d69d9240a061985f2eb20e
injuly <srijannr1@gmail.com>
2022-09-26 23:43:46 +0530

    added pt.js and eslint config

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,12 @@
-export function test() {
-  return 1
-}
+import { CanvasSpace } from 'pts';
+
+const space = new CanvasSpace('#canvas');
+space.setup({ bgcolor: '#fafafa' });
+
+const form = space.getForm();
+space.add(() => {
+    space.bindMouse(true);
+    form.point(space.pointer, 10);
+});
+
+space.play();
