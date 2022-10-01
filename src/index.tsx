7b03f95e04195a882326fedcc8385211a5b96e32
injuly <srijannr1@gmail.com>
2022-10-01 10:54:13 +0530

    can now capture mouse position

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import { CanvasSpace } from 'pts';
+import { CanvasSpace, PtLike } from 'pts';
 
 // preact imports need to exist in the source for the build to work.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { render, h, JSX } from 'preact';
-import { useEffect } from 'preact/hooks';
+import { useCallback, useEffect, useState } from 'preact/hooks';
 import Graph, { FuncPlot } from './graphics/graph';
 import decompose, { approximateFunc } from './math/fourier';
 
@@ -37,11 +37,70 @@ function makeGraphCanvas(id: string, funcsToPlot: FuncPlot[]) {
     graph.plot(form);
   });
 
+  space.bindCanvas('mousedown', () => {
+    console.log('foo');
+  });
+
   space.play();
 }
 
 function Canvas({ width, height }: { width: number; height: number }): JSX.Element {
   return <canvas id="canvas" width={width} height={height}></canvas>;
+}
+
+function initDrawCanvas(id: string): CanvasSpace {
+  const space = new CanvasSpace(id);
+  space.setup({ bgcolor: '#fafafa' });
+  space.bindMouse();
+
+  let isMousePressed = false;
+  space.bindCanvas('mousedown', () => (isMousePressed = true));
+  space.bindCanvas('mouseup', () => (isMousePressed = false));
+
+  const pointsInCurve: PtLike[] = [];
+  let prevTime = -Infinity;
+  const captureIntervalMs = 150;
+  const captureMousePosition = (time: number | undefined) => {
+    if (!time || !isMousePressed) return;
+    const dt = time - prevTime;
+    if (dt >= captureIntervalMs) {
+      prevTime = time;
+      const { pointer } = space;
+      pointsInCurve.push(pointer);
+    }
+  };
+
+  space.add(captureMousePosition);
+  return space;
+}
+
+function DrawCanvas(props: { width: number; height: number }): JSX.Element {
+  const [space, setSpace] = useState<CanvasSpace>();
+
+  // When the component mounts on the DOM, instantiate the space.
+  useEffect(() => {
+    setSpace(initDrawCanvas('draw-canvas'));
+    space?.play();
+  }, []);
+
+  // Once the space is instantiated, stat playing
+  useEffect(() => {
+    space?.play();
+  }, [space]);
+
+  const pauseCanvas = useCallback(() => space?.pause(), []);
+  const playCanvas = useCallback(() => space?.play(), []);
+
+  return (
+    <canvas
+      id="draw-canvas"
+      width={props.width}
+      height={props.height}
+      onMouseOut={pauseCanvas}
+      onMouseUp={pauseCanvas}
+      onMouseDown={playCanvas}
+    ></canvas>
+  );
 }
 
 // Some functions to try:
@@ -78,6 +137,8 @@ function App() {
     <div id="root">
       <Canvas width={400} height={400} />
       {/* Drawing canvas */}
+
+      <DrawCanvas width={400} height={500} />
 
       {/* Recreation canvas */}
     </div>
